347107c5bb318c55b6d105d663b74cbb8c7e657e
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-07-03 23:01:27 +0600

    Insertion Sort

---
+++ insertionsortimp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Size: ");
+    scanf("%i", &n);
+    int ara[n];
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        printf("Value %i: ", i + 1);
+        scanf("%i", &ara[i]);
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        temp = ara[i];
+        int j = i - 1;
+        while (j >= 0 && ara[j] > temp)
+        {
+            ara[j + 1] = ara[j];
+            j--;
+        }
+        ara[j + 1] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i ", ara[i]);
+    }
+}
